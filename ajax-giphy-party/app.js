541983639456083db4a319f069ec7aa1b38a1acc
Kevin Nguyen <kayvinhnguyen@gmail.com>
2023-02-28 17:03:43 -0800

    finished part 2

--- ajax-giphy-party/app.js
+++ ajax-giphy-party/app.js
@@ -1,1 +1,24 @@
-console.log("Let's get this party started!");
+// http://api.giphy.com/v1/gifs/search?q=${inputValue}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym
+
+// get form input value
+// dig thru for img
+
+async function getGiphy(evt) {
+    evt.preventDefault();
+    let urlGiphy = `http://api.giphy.com/v1/gifs/search?q=${getInput()}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`;
+    let giphy = await axios.get(urlGiphy);
+    
+    addGiphy(giphy.data.data[0].images.original.url);
+}
+
+async function addGiphy(url) {
+    let addUrl = `<img src=${url}>`
+    $("#giphy-container").append(addUrl);
+}
+
+function getInput() {
+    return inputValue = $('#form-input').val();
+}
+
+$("#search-button").on('click', getGiphy);
+
